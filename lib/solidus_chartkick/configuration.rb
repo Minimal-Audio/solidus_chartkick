99008b77c31fca78951128d354c0ad27e5ecc313
Jacob Penn <penn.jacob@gmail.com>
2023-02-19 14:14:51 -0800

    moves view logic to controller and creates expression class

--- lib/solidus_chartkick/configuration.rb
+++ lib/solidus_chartkick/configuration.rb
@@ -2,9 +2,7 @@
 
 module SolidusChartkick
   class Configuration
-    # Define here the settings for this extension, e.g.:
-    #
-    # attr_accessor :my_setting
+    attr_accessor :expressions
   end
 
   class << self
