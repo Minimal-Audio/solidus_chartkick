c18c21c6300e5f53cc4ee5854bbdc1ad576298b7
Jacob Penn <penn.jacob@gmail.com>
2023-02-18 18:01:45 -0800

    run solidus extension generator

---
+++ lib/solidus_chartkick.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'solidus_chartkick/configuration'
+require 'solidus_chartkick/version'
+require 'solidus_chartkick/engine'
---
+++ lib/solidus_chartkick/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SolidusChartkick
+  VERSION = '0.0.1'
+end
