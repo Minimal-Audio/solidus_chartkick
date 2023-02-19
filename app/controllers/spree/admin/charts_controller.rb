588d3c7f8e04f36d8346772477f45d41fc02e75b
Jacob Penn <penn.jacob@gmail.com>
2023-02-18 22:23:32 -0800

    adds a functional default view

--- app/controllers/spree/admin/charts_controller.rb
+++ app/controllers/spree/admin/charts_controller.rb
@@ -5,11 +5,13 @@ module Spree
     class ChartsController < Spree::Admin::BaseController
       def index
         # some sensible defaults
+        params[:period] ||= :day
         params[:start_gt] ||= Time.now - 7.days
         params[:end_lt] ||= Time.now
 
-        params[:start_gt] = params[:start_gt].to_datetime.beginning_of_day
-        params[:end_lt] = params[:end_lt].to_datetime.end_of_day
+        params[:period] = params[:period].to_sym
+        params[:start_gt] = params[:start_gt].to_datetime
+        params[:end_lt] = params[:end_lt].to_datetime
       end
     end
   end
