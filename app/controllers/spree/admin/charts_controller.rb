710663f5605d58e846a63434aef9433e2c305c74
Jacob Penn <penn.jacob@gmail.com>
2023-02-18 20:32:58 -0800

    add date filters

--- app/controllers/spree/admin/charts_controller.rb
+++ app/controllers/spree/admin/charts_controller.rb
@@ -3,10 +3,14 @@
 module Spree
   module Admin
     class ChartsController < Spree::Admin::BaseController
-
       def index
-      end
+        # some sensible defaults
+        params[:start_gt] ||= Time.now - 7.days
+        params[:end_lt] ||= Time.now
 
+        params[:start_gt] = params[:start_gt].beginning_of_day
+        params[:end_lt] = params[:end_lt].end_of_day
+      end
     end
   end
 end
